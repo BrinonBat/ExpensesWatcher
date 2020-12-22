0a34b3306ae5137d73da6b91c00f4def90e31001
BrinonBat <baptiste@brinon.eu>
2020-12-22 11:34:33 +0100

    rename

--- app/src/main/java/BaptisteBrinon/moneyWatcher/MainActivity.java
+++ app/src/main/java/BaptisteBrinon/moneyWatcher/MainActivity.java
@@ -5,27 +5,28 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import BaptisteBrinon.moneyWatcher.database.DBHelper;
+import BaptisteBrinon.moneyWatcher.database.ExpenseDBHelper;
 import BaptisteBrinon.moneyWatcher.database.ExpensesContract;
 
 public class MainActivity extends AppCompatActivity {
     static final int ADD_EXPENSE_REQUEST = 1;
-    private SQLiteDatabase database;
+    private SQLiteDatabase expenses_db;
+    private SQLiteDatabase categories_db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //database management
-        DBHelper db_helper = new DBHelper(this);
-        database = db_helper.getWritableDatabase();
-        Cursor cursor = get_all_tasks();
+        //expenses database
+        ExpenseDBHelper db_helper = new ExpenseDBHelper(this);
+        expenses_db = db_helper.getWritableDatabase();
+        Cursor cursor = get_all_expenses();
 
     }
 
-    private Cursor get_all_tasks() {
-        return database.query(
+    private Cursor get_all_expenses() {
+        return expenses_db.query(
                 ExpensesContract.ExpensesEntry.TABLE_NAME,
                 null,
                 null,
@@ -37,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private boolean remove_expense(long id) {
-        return database.delete(ExpensesContract.ExpensesEntry.TABLE_NAME, ExpensesContract.ExpensesEntry._ID + "=" + id, null) > 0;
+        return expenses_db.delete(ExpensesContract.ExpensesEntry.TABLE_NAME, ExpensesContract.ExpensesEntry._ID + "=" + id, null) > 0;
     }
 
 }
