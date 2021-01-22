75ec611ca2ebd520b058143a824d3af33db6ab13
BrinonBat <baptiste@brinon.eu>
2021-01-22 18:00:51 +0100

    added the categories database, yet still have to be tested

--- app/src/main/java/BaptisteBrinon/moneyWatcher/MainActivity.java
+++ app/src/main/java/BaptisteBrinon/moneyWatcher/MainActivity.java
@@ -4,26 +4,52 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import java.util.List;
+
+import BaptisteBrinon.moneyWatcher.database.CategoryDBHelper;
 import BaptisteBrinon.moneyWatcher.database.ExpenseDBHelper;
 import BaptisteBrinon.moneyWatcher.database.ExpensesContract;
 
 public class MainActivity extends AppCompatActivity {
     //static final int ADD_EXPENSE_REQUEST = 1;
+    static final int MANAGE_CATEGORIES_REQUEST = 2;
+    private Spinner spin_categories;
     private SQLiteDatabase expenses_db;
-    //private SQLiteDatabase categories_db;
+    private SQLiteDatabase categories_db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spin_categories = findViewById(R.id.categories);
+        /*****categories database*****/
+        CategoryDBHelper cdb_helper = new CategoryDBHelper(this);
+        List<String> labels = cdb_helper.getAllNames();
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, labels);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        //expenses database
-        ExpenseDBHelper db_helper = new ExpenseDBHelper(this);
-        expenses_db = db_helper.getWritableDatabase();
-        Cursor cursor = get_all_expenses();
+        // attaching data adapter to spinner
+        spin_categories.setAdapter(dataAdapter);
 
+        //https://www.javatpoint.com/android-sqlite-example-with-spinner
+        /* cours
+        categories_db = cdb_helper.getWritableDatabase();
+        Cursor categories_curs = get_all_categories();
+        */
+
+        /*****expenses database*****/
+        ExpenseDBHelper edb_helper = new ExpenseDBHelper(this);
+        expenses_db = edb_helper.getWritableDatabase();
+        Cursor expenses_curs = get_all_expenses();
+
+        //Spinner management
+        spin_categories = (Spinner) findViewById(R.id.categories);
     }
+
+
 
     private Cursor get_all_expenses() {
         return expenses_db.query(
--- app/src/main/java/BaptisteBrinon/moneyWatcher/database/CategoryDBHelper.java
+++ app/src/main/java/BaptisteBrinon/moneyWatcher/database/CategoryDBHelper.java
@@ -1,13 +1,21 @@
 package BaptisteBrinon.moneyWatcher.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoryDBHelper extends SQLiteOpenHelper {
-    //database infos
-    private static final String DATABASE_NAME = "categories.db";
+    //database informations
     private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "categories.db";
+    public static final String TABLE_NAME = "categories";
+    //table
+    public static final String COLUMN_NAME = "name";
 
     public CategoryDBHelper(Context context){
         super(context,DATABASE_NAME,null,DATABASE_VERSION);
@@ -16,18 +24,54 @@ public class CategoryDBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         //table holding data
-        final String SQL_CREATE_WAITLIST_TABLE ="CREATE TABLE " +
-                CategoriesContract.CategoriesEntry.TABLE_NAME + " ("+
-                CategoriesContract.CategoriesEntry.COLUMN_NAME + " FLOAT PRIMARY KEY " +
-                "); ";
-        sqLiteDatabase.execSQL(SQL_CREATE_WAITLIST_TABLE);
+        final String SQL_CREATE_TABLE ="CREATE TABLE " + TABLE_NAME +
+                " ("+ COLUMN_NAME + " FLOAT PRIMARY KEY" + " ); ";
+        sqLiteDatabase.execSQL(SQL_CREATE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         //drop the table and create a new one when the version is changed
         // will be modified to "ALTER TABLE" in the future
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + ExpensesContract.ExpensesEntry.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(sqLiteDatabase);
     }
+
+    public void insertName(String name){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, name);
+
+        // Inserting Row
+        db.insert(TABLE_NAME, null, values);
+        db.close(); // Closing database connection
+    }
+
+    public List<String> getAllNames(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        List<String> list = new ArrayList<String>();
+
+        //get datas
+        String query = "SELECT * FROM " + TABLE_NAME;
+        Cursor cursor = db.rawQuery(query, null);
+        if(cursor.moveToFirst()){
+            do{
+              list.add(cursor.getString(0)); // adding 1st column data (name)
+            } while (cursor.moveToNext());
+        }
+
+        //return the result
+        cursor.close();
+        db.close();
+        return list;
+    }
+
+    private boolean remove_category(String name) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        boolean ok = db.delete(TABLE_NAME, COLUMN_NAME+ "=" + name, null) > 0;
+        db.close();
+        return ok; // so I can handle some errors
+    }
+
 }
